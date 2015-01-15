56d40942a1125d5f4f6bb68cbd2abf61c3a6d48c
Max New <maxsnew@gmail.com>
2015-01-15 12:12:15 -0500

    type bset

--- soft-contracts-games/tetris/typed/bset.rkt
+++ soft-contracts-games/tetris/typed/bset.rkt
@@ -1,96 +1,103 @@
-#lang racket
+#lang typed/racket
 
 (require "data.rkt"
          "block.rkt"
          "consts.rkt")
 
-;; blocks-contains? : BSet Block -> Boolean
 ;; Determine if the block is in the set of blocks.
+(: blocks-contains? (-> BSet Block Boolean))
 (define (blocks-contains? bs b)
-  (ormap (λ (c) (block=? b c)) bs))
+  (ormap (λ: ([c : Block]) (block=? b c)) bs))
 
-;; blocks-subset? : BSet BSet -> Boolean
 ;; is every element in bs1 also in bs2?
+(: blocks-subset? (-> BSet BSet Boolean))
 (define (blocks-subset? bs1 bs2)
-  (andmap (λ (b) (blocks-contains? bs2 b)) bs1))
+  (andmap (λ: ([b : Block]) (blocks-contains? bs2 b)) bs1))
 
-;; blocks=? : BSet BSet -> Boolean
 ;; Determine if given sets of blocks are equal.
+(: blocks=? (-> BSet BSet Boolean))
 (define (blocks=? bs1 bs2)
   (and (blocks-subset? bs1 bs2)
        (blocks-subset? bs2 bs1)))
 
-;; blocks-intersect : BSet BSet -> BSet
 ;; Return the set of blocks that appear in both sets.
+(: blocks-intersect (-> BSet BSet BSet))
 (define (blocks-intersect bs1 bs2)
-  (filter (λ (b) (blocks-contains? bs2 b)) bs1))
+  (filter (λ: ([b : Block]) (blocks-contains? bs2 b)) bs1))
 
-;; blocks-count : BSet -> Nat
 ;; Return the number of blocks in the set.
+(: blocks-count (-> BSet Natural))
 (define (blocks-count bs)
   (length bs))  ;; No duplicates, cardinality = length.
 
-;; blocks-move : Number Number BSet -> BSet
 ;; Move each block by the given X & Y displacement.
+(: blocks-move (-> Real Real BSet BSet))
 (define (blocks-move dx dy bs)
-  (map (λ (b) (block-move dx dy b)) bs))
+  (map (λ: ([b : Block]) (block-move dx dy b)) bs))
 
-;; blocks-rotate-ccw : Posn BSet -> BSet
 ;; Rotate the blocks 90 counterclockwise around the posn.
+(: blocks-rotate-ccw (-> Posn BSet BSet))
 (define (blocks-rotate-ccw c bs)
-  (map (λ (b) (block-rotate-ccw c b)) bs))
+  (map (λ: ([b : Block]) (block-rotate-ccw c b)) bs))
 
-;; blocks-rotate-cw : Posn BSet -> BSet
 ;; Rotate the blocks 90 clockwise around the posn.
+(: blocks-rotate-cw (-> Posn BSet BSet))
 (define (blocks-rotate-cw c bs)
-  (map (λ (b) (block-rotate-cw c b)) bs))
+  (map (λ: ([b : Block]) (block-rotate-cw c b)) bs))
 
-;; blocks-change-color : BSet Color -> BSet
+(: blocks-change-color (-> BSet Color BSet))
 (define (blocks-change-color bs c)
-  (map (λ (b) (block (block-x b) (block-y b) c))
+  (map (λ: ([b : Block]) (block (block-x b) (block-y b) c))
        bs))
 
-;; blocks-row : BSet Number -> BSet
 ;; Return the set of blocks in the given row.
+(: blocks-row (-> BSet Real BSet))
 (define (blocks-row bs i)
-  (filter (λ (b) (= i (block-y b))) bs))
+  (filter (λ: ([b : Block]) (= i (block-y b))) bs))
 
-;; full-row? : BSet Nat -> Boolean
 ;; Are there a full row of blocks at the given row in the set.
+(: full-row? (-> BSet Natural Boolean))
 (define (full-row? bs i)
   (= board-width (blocks-count (blocks-row bs i))))
 
-;; blocks-overflow? : BSet -> Boolean
 ;; Have any of the blocks reach over the top of the board?
+(: blocks-overflow? (-> BSet Boolean))
 (define (blocks-overflow? bs)
-  (ormap (λ (b) (<= (block-y b) 0)) bs))
+  (ormap (λ: ([b : Block]) (<= (block-y b) 0)) bs))
 
-;; blocks-union : BSet BSet -> BSet
 ;; Union the two sets of blocks.
+(: blocks-union (-> BSet BSet BSet))
 (define (blocks-union bs1 bs2)
-  (foldr (λ (b bs)
+  (foldr (λ: ([b  : Block]
+              [bs : BSet])
            (cond [(blocks-contains? bs b) bs]
                  [else (cons b bs)]))
          bs2
          bs1))
 
-;; blocks-max-y : BSet -> Number
 ;; Compute the maximum y coordinate;
 ;; if set is empty, return 0, the coord of the board's top edge.
+(: blocks-max-y (-> BSet Real))
 (define (blocks-max-y bs)
-  (foldr (λ (b n) (max (block-y b) n)) 0 bs))
+  (foldr (λ: ([b : Block]
+              [n : Real])
+           (max (block-y b) n)) 0 bs))
 
-;; blocks-min-x : BSet -> Number
 ;; Compute the minimum x coordinate;
 ;; if set is empty, return the coord of the board's right edge.
+(: blocks-min-x (-> BSet Real))
 (define (blocks-min-x bs)
-  (foldr (λ (b n) (min (block-x b) n)) board-width bs))
+  (foldr (λ: ([b : Block]
+              [n : Real])
+           (min (block-x b) n)) board-width bs))
 
-;; blocks-max-x : BSet -> Number
 ;; Compute the maximum x coordinate;
 ;; if set is empty, return 0, the coord of the board's left edge.
+(: blocks-max-x (-> BSet Real))
 (define (blocks-max-x bs)
-  (foldr (λ (b n) (max (block-x b) n)) 0 bs))
+  (foldr (λ: ([b : Block] 
+              [n : Real])
+           (max (block-x b) n)) 0 bs))
 
 (provide
  blocks-contains?
