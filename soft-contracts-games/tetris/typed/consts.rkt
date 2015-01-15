d463ff7811eaf8a86fd2a10e0548417432280acb
Max New <maxsnew@gmail.com>
2015-01-15 11:56:43 -0500

    Type consts.rkt

--- soft-contracts-games/tetris/typed/consts.rkt
+++ soft-contracts-games/tetris/typed/consts.rkt
@@ -1,10 +1,16 @@
-#lang racket
+#lang typed/racket
 
+(: block-size Integer)
 (define block-size 20)
+
+(: board-height Integer)
 (define board-height 20)
+
+(: board-width Integer)
 (define board-width 10)
-(provide/contract [block-size integer?]
-                  [board-width integer?]
-                  [board-height integer?])
+
+(provide block-size
+         board-width
+         board-height)
 
 
