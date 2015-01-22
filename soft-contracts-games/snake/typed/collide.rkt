cc048bd4400146631c79c17d64b33c6ef3530983
Max New <maxsnew@gmail.com>
2015-01-22 17:35:43 -0500

    type snake/collide

--- soft-contracts-games/snake/typed/collide.rkt
+++ soft-contracts-games/snake/typed/collide.rkt
@@ -1,25 +1,25 @@
-(module collide racket  
+(module collide typed/racket
   (require "data.rkt"
            "const.rkt")
   
-  ;; snake-wall-collide? : Snake -> Boolean
   ;; Is the snake colliding with any of the walls?
+  (: snake-wall-collide? : (Snake . -> . Boolean))
   (define (snake-wall-collide? snk)
     (head-collide? (car (snake-segs snk))))
   
-  ;; head-collide? : Posn -> Boolean
+  (: head-collide? : (Posn . -> . Boolean))
   (define (head-collide? p)
     (or (<= (posn-x p) 0)
         (>= (posn-x p) BOARD-WIDTH)
         (<= (posn-y p) 0)
         (>= (posn-y p) BOARD-HEIGHT)))
   
-  ;; snake-self-collide? : Snake -> Boolean
+  (: snake-self-collide? : (Snake . -> . Boolean))
   (define (snake-self-collide? snk)
     (segs-self-collide? (car (snake-segs snk))
                         (cdr (snake-segs snk))))
   
-  ;; segs-self-collide? : Posn Segs -> Boolean
+  (: segs-self-collide? : (Posn (Listof Posn) . -> . Boolean))
   (define (segs-self-collide? h segs)
     (cond [(empty? segs) #f]
           [else (or (posn=? (car segs) h)
