f5ba5fe810587cf72e80d1712cdaffb843803452
Asumu Takikawa <asumu@ccs.neu.edu>
2015-01-26 15:13:58 -0500

    Make setup-benchmark take an argument directory

--- tools/setup-benchmark.rkt
+++ tools/setup-benchmark.rkt
@@ -4,32 +4,41 @@
     
 ;; Usage:
 ;;   racket setup-benchmark.rkt
-;; will set up benchmarks using the current directory
+;;   racket setup-benchmark.rkt <directory>
+;; will set up benchmarks using the current directory or the
+;; provided one
 (module+ main
-  (create-benchmark-dirs))
+  (match (current-command-line-arguments)
+    [(vector path)
+     (create-benchmark-dirs path)]
+    [_ (create-benchmark-dirs (current-directory))]))
 
-(define (create-benchmark-dirs #:base [base "base"]
-                               #:typed [typed "typed"]
-                               #:untyped [untyped  "untyped"]
+(define (create-benchmark-dirs pwd
+                               #:base [*base "base"]
+                               #:typed [*typed "typed"]
+                               #:untyped [*untyped "untyped"]
                                #:dir-base-name [name "variation"]
-                               #:benchmark-dir [bdir "benchmark"])
-  (define pwd (current-directory))
-  (displayln pwd)
-                               
-  (unless (andmap directory-exists? (list base typed untyped))
-    (error "Invalid Directory."))
-  
+                               #:benchmark-dir [*bdir "benchmark"])
+  (define-values (base typed untyped bdir)
+    (values (build-path pwd *base)
+            (build-path pwd *typed)
+            (build-path pwd *untyped)
+            (build-path pwd *bdir)))
+
+  (for ([dir (in-list (list base typed untyped))])
+    (unless (directory-exists? dir)
+      (raise-user-error "directory ~a does not exist" dir)))
+
   (define typed-dir (directory-list typed))
   (define untyped-dir (directory-list untyped))
   
-  
   (unless (= (length typed-dir) (length untyped-dir))
     (error "Typed and Untyped directories do not have the same number of files"))
-  
+
   (unless (set=? (map path->string typed-dir)
                  (map path->string untyped-dir))
     (error "Typed and Untyped directories do not contain the same file names."))
-  
+
   (define file-names (for/list ([path (in-list typed-dir)]
                                 #:when (regexp-match? #rx"\\.rkt$|\\.ss$" path))
                        (path->string path)))
@@ -37,23 +46,23 @@
 
   (define combinations (build-combinations num-files))
   ;; make benchmark directory
-  (make-directory (build-path pwd bdir))
+  (make-directory bdir)
   (list file-names 
         (for/list ([combo (in-list combinations)])
           ; build target directory
           (define var-dir-num (number->string (combination->number combo)))
-          (define cdir (build-path pwd bdir (string-append name var-dir-num)))
-          (copy-directory/files (build-path pwd base)
+          (define cdir (build-path bdir (string-append name var-dir-num)))
+          (copy-directory/files base
                                 cdir
                                 #:keep-modify-seconds? #t)
           (for ([item (in-list (zip combo file-names))])
             (copy-file
-             (build-path pwd (if (first item) typed untyped) (second item))
+             (build-path (if (first item) typed untyped) (second item))
              (build-path cdir (second item)))
             ;; special case for acquire
             #;(when (string=? (second item) "tree.rkt")
               (copy-file
-               (build-path pwd "conditional" (if (first item) typed untyped) "typed-wrapper.rkt")
+               (build-path "conditional" (if (first item) typed untyped) "typed-wrapper.rkt")
                (build-path cdir "typed-wrapper.rkt"))))
           cdir)))  
 
