a79ba0a20c6e90bcc22618e83424018b8207efb7
Asumu Takikawa <asumu@ccs.neu.edu>
2015-01-27 18:59:50 -0500

    Move files into packages, add a run script

---
+++ tools/benchmark-util/main.rkt
@@ -0,0 +1,4 @@
+#lang racket/base
+
+(require "require-typed-utils.rkt")
+(provide (all-from-out "require-typed-utils.rkt"))
---
+++ tools/run.rkt
@@ -0,0 +1,31 @@
+#lang racket/base
+
+;; Benchmark driver
+
+(require racket/cmdline
+         racket/match
+         racket/system)
+
+(module+ main
+  (match-define (list basepath entry-point num-iterations)
+    (command-line #:program "benchmark-runner"
+                  #:args (basepath entry-point num-iterations)
+                  (list basepath entry-point num-iterations)))
+  (unless (path-string? basepath)
+    (raise-user-error (format "expected a path, given ~a" basepath)))
+  (unless (and (path-string? entry-point)
+               (regexp-match? #rx"\\.rkt$" entry-point))
+    (raise-user-error (format "expected a Racket file, given ~a" entry-point)))
+  (define iters (string->number num-iterations))
+  (unless (number? iters)
+    (raise-user-error (format "expected a number, given ~a" num-iterations)))
+
+  (define variations
+    (for/list ([file (in-list (directory-list (build-path basepath "benchmark")))])
+      (build-path basepath "benchmark" file entry-point)))
+
+  (for ([var (in-list variations)])
+    (for ([i (in-range iters)])
+      (printf "iteration #~a of ~a~n" i var)
+      ;; FIXME: use benchmark library
+      (time (system (string-append "racket " (path->string var)))))))
