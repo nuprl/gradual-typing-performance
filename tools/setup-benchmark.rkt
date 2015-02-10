289f3bd001bceaf7cbb3e7c3b3e628bf0d277514
Matthias Felleisen <matthias@ccs.neu.edu>
2015-02-10 18:51:22 -0500

    revised setup some more so that directory tells us typed vs untyped (a bit)

--- tools/setup-benchmark.rkt
+++ tools/setup-benchmark.rkt
@@ -15,6 +15,11 @@
 ;; ===================================================================================================
 (require srfi/13)
 
+(define TYPED? "typed?")
+(define TYPED "yes")
+(define UNTYPED "no")
+(define COMMENT (string-append ";; " (make-string 77 #\-)))
+
 (define (create-benchmark-dirs 
          pwd
          #:base [base (build-path pwd "base")]
@@ -38,48 +43,47 @@
   ;; -------------------------------------------------------------------------------------------------
   ;; WORK: 
   
-  (set-up-benchmark-directory bdir)
-  (create-populate-base-directory base bdir)
-  (define generated-variations
-    (create-populate-variations-directories* variation bdir typed untyped typed-dir))
-  ;; throw them away: 
-  (void))
-
-;; String Path Path Path [Listof String] -> [Listof String]
-(define (create-populate-variations-directories* name bdir typed untyped typed-dir)
-  (define file-names
+  (define file-names* ;; because typed-dir and untyped-dir contains same file names 
     (for/list ([path (in-list typed-dir)] #:when (regexp-match? #rx"\\.rkt$|\\.ss$" path))
       (path->string path)))
   
-  (for/list ([c (in-list (build-combinations (length file-names)))])
-    (define cdir (build-path bdir (format "~a~a" name (combination->number c))))
+  (set-up-benchmark-directory bdir)
+  (create-populate-base-directory base bdir)
+  (create-populate-variations-directories* variation bdir typed untyped file-names*)
+  ;; throw them away: 
+  (void))
+
+;; ---------------------------------------------------------------------------------------------------
+;; String Path Path Path [Listof String] -> [Listof String]
+(define (create-populate-variations-directories* name bdir typed untyped file-names*)
+  (for/list ([combination (in-list (build-combinations* (length file-names*)))])
+    (define cdir (build-path bdir (apply string-append "variation" (map number->string combination))))
     (make-directory cdir)
-    ;; populate cdir & generate a README file in S-expression form that specifies the origin of files
-    (with-output-to-file (build-path cdir "README")
-      (lambda ()
-        (displayln COMMENT)
-        (printf ";; ~a ~a\n" (filled "file") TYPED?)
-        (displayln COMMENT)
-        (displayln "(")
-        (for ([file-name (in-list file-names)][typed? (in-list c)])
-          (printf "(~a ~a)\n" (filled file-name) (if typed? "yes" "no"))
-          (copy-file (build-path (if typed? typed untyped) file-name) (build-path cdir file-name))
-          #;
-          (when (string=? file-name "tree.rkt")
-            ;; special case for Acquire:
-            (copy-file
-             (build-path "conditional" (if typed? typed untyped) "typed-wrapper.rkt")
-             (build-path cdir "typed-wrapper.rkt"))))
-        (displayln ")")))
+    (create-readme cdir (populate-variation cdir file-names* combination typed untyped))
     cdir))
 
-(define TYPED? "typed?")
-(define COMMENT (string-append ";; " (make-string 77 #\-)))
+;; ---------------------------------------------------------------------------------------------------
+;; Path [Listof [List String Boolean]] -> Void 
+(define (create-readme cdir population)
+  (with-output-to-file (build-path cdir "README") (lambda () (pretty-print population))))
 
+;; ---------------------------------------------------------------------------------------------------
+;; Path [Listof String] Combination Path Path -> Void 
+(define (populate-variation cdir file-names* combination typed untyped)
+  (for/list ([file-name (in-list file-names*)][src (in-list combination)])
+    (copy-file (build-path (if (typed? src) typed untyped) file-name) (build-path cdir file-name))
+    #;
+    (special-case-for-acquire file-name typed? typed untyped cdir)
+    (list file-name (typed? src))))
+
+(define typed? (compose not zero?))
+
+;; ---------------------------------------------------------------------------------------------------
 ;; String -> String 
 (define (filled s)
   (string-pad-right s (- 80 (string-length TYPED?))))
 
+;; ---------------------------------------------------------------------------------------------------
 ;; Path -> Void 
 (define (set-up-benchmark-directory bdir)
   (when (directory-exists? bdir)
@@ -87,43 +91,47 @@
     (delete-directory/files bdir))
   (make-directory bdir))
 
+;; ---------------------------------------------------------------------------------------------------
 ;; Path Path -> Void 
 (define (create-populate-base-directory base bdir)
   (copy-directory/files base (build-path bdir "base") #:keep-modify-seconds? #t))
 
+;; ---------------------------------------------------------------------------------------------------
 ;; [Listof Path] [Listof Path] -> Void
 (define (same-file-names? typed-dir untyped-dir)
   (unless (set=? (map path->string typed-dir) (map path->string untyped-dir))
     (error "Typed and Untyped directories do not contain the same file names.")))
 
+;; ---------------------------------------------------------------------------------------------------
 ;; [Listof X] [Listof Y] -> Void
 (define (same-number-of-files? typed-dir untyped-dir)
   (unless (= (length typed-dir) (length untyped-dir))
     (error "Typed and Untyped directories do not have the same number of files")))
 
+;; ---------------------------------------------------------------------------------------------------
 ;; String *-> Boolean 
 (define (files-exist? . dir*)
   (for ([dir (in-list dir*)])
     (unless (directory-exists? dir)
       (raise-user-error 'setup-benchmark "directory `~a' does not exist" dir))))
 
-;; a combination is a (listof boolean) 
+;; ---------------------------------------------------------------------------------------------------
+;; Combination = [Listof {0|1}]
+
 ;; Integer -> [Listof Combination]
-(define (build-combinations n)
+(define (build-combinations* n)
   [when (< n 1) (error "No combinations of length 0")]
   (let build-combinations ([n n])
     (cond
-      [(= n 1) (list (list #f) (list #t))]
+      [(= n 1) (list (list 0) (list 1))]
       [else (let ([last (build-combinations (sub1 n))])
-              (append (map (curry cons #f) last)
-                      (map (curry cons #t) last)))])))
+              (append (map (curry cons 0) last) (map (curry cons 1) last)))])))
 
-;; Combination -> Integer 
-(define (combination->number combination)
-  (let run ([combo (reverse combination)])
-    (cond
-      [(empty? combo) 0]
-      [else (define result (run (rest combo)))
-            (if (first combo) (+ 1 (* 2 result)) (* 2 result))])))
+;; ---------------------------------------------------------------------------------------------------
 
-
+(define (special-case-for-acquire file-name typed? typed untyped cdir)
+  (when (string=? file-name "tree.rkt")
+    ;; special case for Acquire:
+    (copy-file
+     (build-path "conditional" (if typed? typed untyped) "typed-wrapper.rkt")
+     (build-path cdir "typed-wrapper.rkt"))))
