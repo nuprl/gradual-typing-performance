09e43d69f93313a3d9547fd9394769c2349f1b03
ben <types@ccs.neu.edu>
2015-04-07 18:09:54 -0400

    [viz] simple labels for module graph

--- tools/visualization/module-graph.py
+++ tools/visualization/module-graph.py
@@ -7,11 +7,15 @@ Input should be two files
 
 Checklist:
 - [X] create one module graph
-- [ ] color edges by num unacceptable configs active in
-- [ ] label edges by avg, med runtime
+- [X] label edges by num unacceptable configs active in
+- [X] label edges by avg, med runtime
 - [ ] repeat for all files
+- [ ] more clever label representations
+- [ ] typed vs untyped
 - [ ] try representing / identifying clusters
 """
+
+### imports
 
 import util
 import statistics
@@ -21,8 +25,10 @@ import matplotlib
 matplotlib.use('Agg') # Disable the display, does not affect graph generation
 import matplotlib.pyplot as plt
 
+### constants
+
 # Factor away from minumum runtime a path must be to be considered "unacceptably bad"
-UNACCEPTABLE = 20
+UNACCEPTABLE = 10
 # Separator for input files
 SEP = "\t"
 
@@ -71,9 +77,9 @@ def gen_unacceptable(fname):
     with open(fname, "r") as f:
         next(f)
         for line in f:
-            data = line.strip().split(SEP)[1::]
+            data = [int(x) for x in line.strip().split(SEP)[1::]]
             runtime = statistics.mean(data)
-            min_time = min(min_time, runtime)
+            min_time = runtime if min_time is None else min(min_time, runtime)
     very_bad_runtime = min_time * UNACCEPTABLE
     return (lambda x: x > very_bad_runtime)
 
@@ -101,37 +107,61 @@ def module_graph_init(fname):
 def edge_labels_by_runtime(g, fname, agg_f):
     # Generate edge labels by collecting the runtimes
     # of all data samples where the edge is present, and
-    # aggregating these values with `agg_f`
-    raise NotImplementedError("oh noes")
+    # aggregating these values with `agg_f : (-> (List Nat) Nat)`
+    labels = {}
+    for (u,v) in g.edges_iter():
+        # Collect node's index, skip if there is no index
+        if ('index' not in g.node[u]) or ('index' not in g.node[v]):
+            continue
+        index1 = g.node[u]['index']
+        index2 = g.node[v]['index']
+        runtimes = []
+        # For each edge, go through the data file and collect all runtimes
+        with open(fname, "r") as dataf:
+            next(dataf)
+            for line in dataf:
+                rows = line.strip().split(SEP)
+                title = rows[0]
+                # If this edge is a typed/untyped boundary...
+                if title[index1] != title[index2]:
+                    runtimes.extend([int(x) for x in rows[1::]])
+        labels[(u,v)] = int(agg_f(runtimes))
+    return labels
 
 def save_graph(g, fname, tag, edge_labels=None):
     new_name = util.gen_name(fname, tag, "png")
     plt.title(new_name.rsplit(".", 1)[0].rsplit("/", 1)[-1])
-    pos = nx.spring_layout(g)
-    # pos = nx.spectral_layout(g)
+    # pos = nx.spring_layout(g)
+    pos = nx.spectral_layout(g)
     nx.draw_networkx_nodes(g, pos, node_color="b", node_size=1000, alpha=0.6)
     nx.draw_networkx_edges(g, pos, width=1, alpha=0.5)
     labels = dict(( (str(node), str(node) ) for node in g.nodes_iter()))
     nx.draw_networkx_labels(g, pos, labels)
     if edge_labels is not None:
-        mx.draw_networkx_edge_labels(g, pos, edge_labels)
+        nx.draw_networkx_edge_labels(g, pos, edge_labels, label_pos=0.5)
     plt.axis("off")
     plt.savefig(new_name)
+    plt.clf()
     print("Saved graph to '%s'" % new_name)
     return new_name
 
 def main(fname, graph_name):
-    # Create a module graph,
-    # Build and save figures.
+    # Create a module graph, build & save figures
     g = module_graph_init(graph_name)
+    ## Simple measures
+    # - normal, boring module graph
     save_graph(g,fname,"modules")
-    # el_mean = edge_labels_by_runtime(g, fname, statistics.mean)
-    # save_graph(g,fname,"edges-avg", el_mean)
-    # el_median = edge_labels_by_runtime(g, fname, statistics.median)
-    # save_graph(g,fname,"edges-med", el_median)
-    # unacceptable = gen_unacceptable(fname)
-    # el_badconfs = edge_labels_by_runtime(g, fname, (lambda xs: sum((1 for x in xs if unacceptable(x)))))
-    # save_graph(g, fname, el_badconfs)
+    # - Label edges by mean, median runtimes
+    el_mean = edge_labels_by_runtime(g, fname, statistics.mean)
+    save_graph(g,fname,"edges-avg", el_mean)
+    el_median = edge_labels_by_runtime(g, fname, statistics.median)
+    save_graph(g,fname,"edges-med", el_median)
+    # - Label edges by number of 'unacceptable' configs they're in
+    unacceptable = gen_unacceptable(fname)
+    unaccept_measure = lambda xs: sum((1 for x in xs if unacceptable(x)))
+    el_badconfs = edge_labels_by_runtime(g, fname, unaccept_measure)
+    save_graph(g, fname, "edges-over-%d" % UNACCEPTABLE, el_badconfs)
+    ## More interesting measures: TODO
     return None
 
 if __name__ == "__main__":
