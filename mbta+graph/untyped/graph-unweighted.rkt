3b5dec7a765188ad0fc50ce6801e36fed5b89b08
ben <types@ccs.neu.edu>
2015-04-18 21:36:53 -0400

    [mbta+graph] init untyped version

---
+++ mbta+graph/untyped/graph-unweighted.rkt
@@ -0,0 +1,40 @@
+#lang racket/base
+
+(require
+ "graph-adjlist-utils.rkt")
+
+(require
+ (only-in racket/set in-set)
+)
+
+; unweighted, adjacency-list graph
+
+(provide
+ unweighted-graph/directed
+ (struct-out unweighted-graph))
+
+(struct unweighted-graph
+        (get-vertices
+         in-neighbors))
+
+
+;; directed graph constructor
+(define (unweighted-graph/directed es)
+  ;; Set up edges
+  (define adj (make-hash))
+  (for ([e es]) 
+    (cond [(list? e) (apply add-edge@ adj e)
+                     (add-vertex@ adj (cadr e))]
+          [else (add-vertex@ adj e)]))
+  ;; Define graph functions
+  (define (get-vertices) (hash-keys adj))
+  (define (in-neighbors v)
+    (in-set 
+     (hash-ref adj v 
+               (λ () (error 'in-vertices "vertex not in graph")))))
+  ;; returns edges as a sequence
+  ;; Create graph
+  (unweighted-graph
+   get-vertices
+   in-neighbors
+))
---
+++ mbta+graph/untyped/main.rkt
@@ -0,0 +1,63 @@
+#lang racket
+
+;; stress testing run-t on 100 look ups, plus 5 [dis|en]ables
+
+;; ===================================================================================================
+(require "run-t.rkt")
+
+;; Nat -> Void 
+;; run the stress test n times
+(define (stress-test n)
+  (for ((_i (in-range n)))
+    (define-values (in out) (make-pipe))
+    (define-values (_in _out) (make-pipe))
+    (define c (make-custodian))
+    (parameterize ([current-custodian c])
+      (start-server in _out)
+      (parameterize ([current-input-port _in]
+                     [current-output-port out])
+        ;; (assert (path "Airport" "Northeastern" 30))
+        ;; (assert (able "dis" "Government"))
+        ;; (assert (path "Airport" "Northeastern" 10))
+        ;; (assert (able "en" "Government"))
+        ;; (assert (path "Airport" "Harvard Square" 20))
+        ;; (assert (able "dis" "Park"))
+        ;; (assert (path "Northeastern" "Harvard Square" 20))
+        ;; (assert (able "en" "Park"))
+        (assert (path "Northeastern" "Harvard Square" 20))))
+    (custodian-shutdown-all c)))
+
+(define (start-server in _out)
+  (thread
+   (lambda ()
+     (let loop ()
+       (run-t in _out)
+       (loop)))))
+
+;; String String Nat -> [Listof String]
+(define (path from to n) 
+  (for/list ([_ (in-range n)]) 
+            (printf "from ~a to ~a\n" from to)
+            (read-to EOM)))
+
+;; Symbol String -> Void
+(define (able dis-en s) 
+  (printf "~aable ~a\n" dis-en s)
+  (read-to EOM))
+
+(define-syntax assert
+  (syntax-rules ()
+    [(_ (f x y n)) (unless (= (length (f x y n)) n) (error '(f x y n)))]
+    [(_ (f x y)) 
+     (let ([r (f x y)])
+       (unless (= (length r) 1) (*debug `((f x y) ,r))))]))
+
+;; -> [Listof String]
+;; read up to x and collect lines into list
+(define (read-to x)
+  (define next (read-line))
+  (if (or (eof-object? next) (string=? x (string-trim next)))
+      '()
+      (cons next (read-to x))))
+
+(time (stress-test 1))
