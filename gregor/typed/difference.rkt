b9a8d348657f53bc8131e219acb3e5c1ecc588db
ben <types@ccs.neu.edu>
2015-04-26 02:01:04 -0400

    [gregor] one last contract error

--- gregor/typed/difference.rkt
+++ gregor/typed/difference.rkt
@@ -28,7 +28,7 @@
     [date (->* (Natural) (Month Natural) Date)]
 )
 (require/typed/check "datetime.rkt"
-    [datetime<? (-> Date Date Boolean)]
+    [datetime<? (-> DateTime DateTime Boolean)]
     [datetime->date (-> DateTime Date)]
     [date+time->datetime (-> Date Time DateTime)]
     [datetime->time (-> DateTime Time)]
