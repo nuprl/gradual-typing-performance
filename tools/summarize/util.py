d7b5e4d910df99dfba26e4217bb78034051e27d3
ben <types@ccs.neu.edu>
2015-04-30 14:13:48 -0400

    [sample] directory searching helpers

--- tools/summarize/util.py
+++ tools/summarize/util.py
@@ -3,6 +3,7 @@ Generic utility functions
 """
 
 import constants
+import os
 
 def fold_file(fname, acc, fn, ignore_first_line=True):
     """
@@ -28,3 +29,24 @@ def strip_directory(fname):
         Remove everything before the rightmost "/" in the string `fname`
     """
     return fname.rsplit("/", 1)[-1]
+
+def contains_any_directories(dirname):
+    """
+        Returns true if the directory `dirname`
+        contains any sub-directories
+    """
+    return any((True for name in os.listdir(dirname)
+                if os.path.isdir(name)))
+
+def get_files(dirname):
+    """
+        Return the files (not directories) contained in `dirname`
+    """
+    return (name for name in os.listdir(dirname) if os.path.isfile(name))
+
+def count_files(dirname):
+    """
+        Return the number of files in the directory `dirname`
+    """
+    return sum((1 for name in os.listdir(dirname)
+                if os.path.isfile(name)))
