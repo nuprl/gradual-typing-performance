e045fcb838bf3ed1bbb74a31323442229dc36a99
Sam Tobin-Hochstadt <samth@cs.indiana.edu>
2015-04-30 17:31:01 -0400

    Clean up this code and make it work for relative paths.

--- tools/summarize/sexp-to-tab.rkt
+++ tools/summarize/sexp-to-tab.rkt
@@ -13,40 +13,15 @@
 
 ;; ----------------------------------------------------------------------------
 
-(require
-  racket/file
-  racket/string
-)
+(require racket/file
+         racket/string
+         racket/format
+         racket/math)
 
-;; Get the base-2 log of a number
-(define (log2 n)
-  (log2-aux n 1))
-(define (log2-aux n acc)
-  (cond [(= n (expt 2 acc)) acc]
-        [else (log2-aux n (add1 acc))]))
+(define (log2 n) (exact-ceiling (/ (log n) (log 2))))
 
 ;; Convert a natural to a binary string, padded to the supplied width
-(define (natural->binary n pad-width)
-  (string-reverse (nat->bin-aux n (make-string pad-width #\0))))
-(define (nat->bin-aux n str)
-  (cond [(= n 0) str]
-        [else (nat->bin-aux (sub1 n) (incr-bit str))]))
-
-;; Increment a string representation of a binary number
-(define (incr-bit str)
-  (incr-bit-aux str 0)
-  str)
-(define (incr-bit-aux str i)
-  (cond [(>= i (string-length str)) (void)] ;; Silently fail
-        [(eq? #\0 (string-ref str i))
-         (string-set! str i #\1)]
-        [else
-         (string-set! str i #\0)
-         (incr-bit-aux str (add1 i))]))
-
-;; Reverse a string
-(define (string-reverse str)
-  (list->string (reverse (string->list str))))
+(define (natural->binary n pad-width) (~r n #:base 2 #:min-width pad-width #:pad-string "0"))
 
 ;; Read in-file as a vector, transpose data & print
 (define (copy-data-transpose in-file out-file)
@@ -55,14 +30,17 @@
     (lambda ()
       ;; First print titles
       (define num-configs (vector-length vec))
-      (define configs (for/list ([n (in-range num-configs)]) (natural->binary n (log2 num-configs))))
-      (displayln (string-join (cons "Run" configs) "\t"))
+      (define len (log2 num-configs))
+      (display "Run")
+      (for ([n num-configs])
+        (display (natural->binary n len)))
+      (newline)
       ;; For each run
-      (for ([n (in-range (length (vector-ref vec 0)))])
+      (for ([n (length (vector-ref vec 0))])
+        (display n)
         ;; Get the n-th data value from each row
-        (define vals (for/list ([row vec]) (format "~a" (list-ref row n))))
-        ;; Pre-pend run number and print
-        (displayln (string-join (cons (number->string n) vals) "\t"))))))
+        (for ([row vec]) (printf "\t~a" (list-ref row n)))
+        (newline)))))
 
 (define (copy-data in-file out-file)
   (define vec (file->value in-file))
@@ -71,25 +49,18 @@
       ;; First print index
       (define num-configs (vector-length vec))
       (define num-runs (length (vector-ref vec 0)))
-      (displayln (string-join (cons "Run" (for/list ([n (in-range num-runs)]) (format "~a" (add1 n)))) "\t"))
-      ;; Start counter for configurations
-      (define N (make-string (log2 num-configs) #\0))
+      (display "Run")
+      (for ([n num-runs])
+        (printf "\t~a" (add1 n)))
+      (newline)
       ;; For each row, print the config ID and all the values
-      (for ([row vec])
-        (displayln (string-join (cons (string-reverse N) (for/list ([v row]) (format "~a" v))) "\t"))
-        (incr-bit N)))))
+      (for ([(row n) (in-indexed vec)])
+        (display (natural->binary n (log2 num-configs)))
+        (for ([v row]) (printf "\t~a" v))
+        (newline)))))
 
-;; Replace the characters after the "." in the first arguemnt with the second
-(define (replace-extension str ext)
-  (string-append (car (string-split str ".")) "." ext))
-
-;; Parse command-line argument
-(define (main args)
-  (cond [(= 1 (vector-length args))
-         (define in-file (vector-ref args 0))
-         (define out-file (replace-extension in-file "tab"))
-         (copy-data in-file out-file)]
-        [else (displayln "Usage: sexp-to-tab.rkt FILENAME")]))
-
-(main (current-command-line-arguments))
+(module+ main 
+  (require racket/cmdline)
+  (command-line #:args (filename)
+                (copy-data filename (path-replace-suffix filename ".tab"))))
 
