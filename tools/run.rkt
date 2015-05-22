3a01d83704e102d8fff3fdd904ad23c9f3d12ca9
Asumu Takikawa <asumu@ccs.neu.edu>
2015-05-22 18:47:00 -0400

    Adjust benchmark runner to allow parallel runs
    The new -j argument will parallelize the benchmark run
    with however many processes are specified. Note that the
    benchmark script runs in a separate thread, so the -j
    argument should be one less than the number of cores
    you want to use.
    
    Each process is pinned to a single CPU using the CPU
    affinity flag. The script itself is always pinned to
    CPU0.

--- tools/run.rkt
+++ tools/run.rkt
@@ -4,6 +4,7 @@
 
 (require "data-lattice.rkt"
          math/statistics
+         mzlib/os
          pict
          racket/class
          racket/cmdline
@@ -11,7 +12,8 @@
          racket/match
          racket/port
          racket/system
-         racket/vector)
+         racket/vector
+         unstable/sequence)
 
 ;; Flag to set compilation-only mode. If #f it will compile and
 ;; run, otherwise only compiles.
@@ -20,6 +22,11 @@
 ;; The number of times to run the benchmark as a string (to
 ;; be parsed later)
 (define num-iterations (make-parameter "1"))
+
+;; The number of jobs to spawn for the variations. When jobs is
+;; greater than 1, the variation space is split evenly and allocated
+;; to the various jobs.
+(define num-jobs (make-parameter "1"))
 
 ;; Paths to write results/diagrams
 (define output-path (make-parameter #f))
@@ -37,60 +44,83 @@
 ;; Run the variations for each variation directory
 ;; Optional argument gives the exact variation to run.
 ;; Default is to run all variations (either run all, or exactly one)
-;; (Listof Path) Nat [(U (Listof String) #f)] -> Results
-(define (run-benchmarks basepath entry-point iters #:config [cfg #f])
+;; (Listof Path) Path Nat Nat [(U (Listof String) #f)] -> Results
+(define (run-benchmarks basepath entry-point iters jobs #:config [cfg #f])
   (define variations
     (if cfg
       (list (build-path basepath "benchmark" (string-append "variation" cfg) entry-point))
       (mk-variations basepath entry-point)))
   (define results (make-vector (length variations)))
-  (for ([(var var-idx) (in-indexed (in-list variations))])
-    (define-values (new-cwd file _2) (split-path var))
-    (define times null)
+  ;; allocate a slice of the variations per job
+  (define slice-size (ceiling (/ (length variations) jobs)))
+  (define threads
+    (for/list ([var-slice (in-slice slice-size (in-values-sequence (in-indexed (in-list variations))))]
+               [job# (in-range 1 (add1 jobs))])
+      ;; Spawn a control thread for each job, which will in turn spawn an OS process.
+      ;; Each job gets assigned to the CPU that is the same as the job# using
+      ;; the `taskset` command.
+      (thread
+       (λ ()
+         (for ([var-in-slice var-slice])
+           (match-define (list var var-idx) var-in-slice)
+           (define-values (new-cwd file _2) (split-path var))
+           (define times null)
 
-    (parameterize ([current-directory new-cwd])
-      ;; first compile the variation
-      (unless (system (string-append "raco make -v " (path->string file)))
-        (error (format "Compilation failed for '~a/~a', shutting down" (current-directory) (path->string file))))
+           (parameterize ([current-directory new-cwd])
+             ;; first compile the variation
+             (unless (system (format "taskset -c ~a raco make -v ~a"
+                                     job# (path->string file)))
+               (error (format "Compilation failed for '~a/~a', shutting down"
+                              (current-directory) (path->string file))))
 
-      ;; run an extra run of the variation to throw away in order to avoid
-      ;; OS caching issues
-      (unless (only-compile?)
-        (displayln "throwaway build/run to avoid OS caching")
-        (match-define (list _ _ _ _ control)
-          (process (format "racket ~a" (path->string file))))
-        ;; make sure to block on this run
-        (control 'wait))
+             ;; run an extra run of the variation to throw away in order to
+             ;; avoid OS caching issues
+             (unless (only-compile?)
+               (printf "job#~a, throwaway build/run to avoid OS caching~n" job#)
+               (match-define (list in out _ err control)
+                 (process (format "taskset -c ~a racket ~a"
+                                  job# (path->string file))))
+               ;; make sure to block on this run
+               (control 'wait)
+               (close-input-port in)
+               (close-input-port err)
+               (close-output-port out))
 
-      ;; run the iterations that will count for the data
-      (unless (only-compile?)
-        (for ([i (in-range iters)])
-          (printf "iteration #~a of ~a~n" i var)
-          ;; FIXME: use benchmark library
-          (define command `(time (dynamic-require ,(path->string file) #f)))
-          (match-define (list in out pid err proc)
-            (process (format "racket -e '~s'" command)
-                     #:set-pwd? #t))
-          ;; if this match fails, something went wrong since we put time in above
-          (define time-info
-            (for/or ([line (in-list (port->lines in))])
-              (regexp-match #rx"cpu time: (.*) real time: (.*) gc time: (.*)" line)))
-          (match time-info
-            [(list full (app string->number cpu)
-                        (app string->number real)
-                        (app string->number gc))
-             (printf "cpu: ~a real: ~a gc: ~a~n" cpu real gc)
-             (set! times (cons real times))]
-            [#f (void)])
-          ;; print anything we get on stderr so we can detect errors
-          (for-each displayln (port->lines err))
-          ;; we're reponsible for closing these
-          (close-input-port in)
-          (close-input-port err)
-          (close-output-port out))))
-    ;; the order of runs doesn't really matter, but keep them in the order
-    ;; they were run anyway just in case
-    (vector-set! results var-idx (reverse times)))
+             ;; run the iterations that will count for the data
+             (unless (only-compile?)
+               (for ([i (in-range iters)])
+                 (printf "job#~a, iteration #~a of ~a started~n" job# i var)
+                 (define command `(time (dynamic-require ,(path->string file) #f)))
+                 (match-define (list in out pid err control)
+                   (process (format "taskset -c ~a racket -e '~s'" job# command)
+                            #:set-pwd? #t))
+                 ;; if this match fails, something went wrong since we put time in above
+                 (define time-info
+                   (for/or ([line (in-list (port->lines in))])
+                     (regexp-match #rx"cpu time: (.*) real time: (.*) gc time: (.*)" line)))
+                 (match time-info
+                   [(list full (app string->number cpu)
+                               (app string->number real)
+                               (app string->number gc))
+                    (printf "job#~a, iteration#~a - cpu: ~a real: ~a gc: ~a~n"
+                            job# i cpu real gc)
+                    (set! times (cons real times))]
+                   [#f (void)])
+                 ;; print anything we get on stderr so we can detect errors
+                 (for-each displayln (port->lines err))
+                 ;; block on the run, just in case
+                 ;; (the blocking of `port->lines` should be enough)
+                 (control 'wait)
+                 ;; we're reponsible for closing these
+                 (close-input-port in)
+                 (close-input-port err)
+                 (close-output-port out))))
+
+           ;; the order of runs doesn't really matter, but keep them in the order
+           ;; they were run anyway just in case
+           (vector-set! results var-idx (reverse times)))))))
+  ;; synchronize on all jobs
+  (for ([thd (in-list threads)]) (thread-wait thd))
   results)
 
 (module+ main
@@ -111,6 +141,9 @@
                   [("-e" "--entry-point") e-p
                                           "The main file to execute. (Defaults to 'main.rkt'.)"
                                           (entry-point-param e-p)]
+                  [("-j" "--jobs") j
+                                   "The number of processes to spawn"
+                                   (num-jobs j)]
                   #:multi
                   [("-i" "--iterations") n-i
                                          "The number of iterations to run"
@@ -139,8 +172,15 @@
   (define iters (string->number (num-iterations)))
   (unless (number? iters)
     (raise-user-error (format "expected a number, given ~a" (num-iterations))))
+  (define jobs (string->number (num-jobs)))
+  (unless (number? jobs)
+    (raise-user-error (format "expected a number, given ~a" (num-jobs))))
 
-  (define results (run-benchmarks basepath entry-point iters #:config (exclusive-config)))
+  ;; Set the CPU affinity for this script to CPU0. Jobs spawned by this script run
+  ;; using CPU1 and above.
+  (system (format "taskset -pc 0 ~a" (getpid)))
+
+  (define results (run-benchmarks basepath entry-point iters jobs #:config (exclusive-config)))
 
   (when (output-path)
     (with-output-to-file (output-path)
