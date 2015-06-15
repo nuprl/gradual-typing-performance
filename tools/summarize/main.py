0c22f4f97e02533e218266aa3d58cd46326466ad
ben <types@ccs.neu.edu>
2015-06-15 18:00:09 -0400

    [poplpics] main handles multiple files, and compiles results (a hack)

--- tools/summarize/main.py
+++ tools/summarize/main.py
@@ -11,39 +11,54 @@ import constants
 import os
 import sys
 import util
+from LmnSummary import LmnSummary
 from TabfileSummary import TabfileSummary
 from SrsSummary import SrsSummary
 
-def init():
+import latex
+
+def init(out_dir):
    """
        Pre-processing step.
    """
-   if not os.path.exists(constants.OUTPUT_DIR):
-       os.mkdir(constants.OUTPUT_DIR)
+   if not os.path.exists(out_dir):
+       os.mkdir(out_dir)
    return
 
 def main(input_file, sample_size=50, iters=50, verbose=0):
-    """ (-> (Listof Any) (Dictof String Any) Void)
+    """ (-> Path-String (Dictof String Any) Void)
         Collect summary information from the input args by dispatching to the
         appropriate helper function.
         Pretty-print and save results.
     """
+    print("Processing '%s'" % input_file)
     summary = None
-    init()
+    tag = util.strip_suffix(input_file).rsplit("/", 1)[-1]
+    out_dir = "%s-%s" % (constants.OUTPUT_DIR, tag)
+    init(out_dir)
     if input_file.endswith(".rktd") or input_file.endswith(".tab"):
-       summary = TabfileSummary(input_file)
+        # summary = TabfileSummary(input_file)
+        summary = LmnSummary(input_file, out_dir=out_dir)
     elif os.path.isdir(input_file):
         # Sampling mode!
         summary = SrsSummary(input_file, sample_size=sample_size, num_iters=iters)
     else:
         print("Cannot read input file '%s'" % input_file)
         return
-    out_file = "%s/%s.tex" % (constants.OUTPUT_DIR, util.strip_suffix(input_file).rsplit("/", 1)[-1])
+    out_file = "%s/%s.tex" % (out_dir, tag)
     out_port = open(out_file, "w")
     summary.render(out_port)
     out_port.close()
     print("Results saved as '%s'" % out_file)
+    ## BEGIN HACKS
+    cwd = os.getcwd()
+    if os.system("cd %s; xelatex %s; cd %s" % (out_dir, tag, cwd)):
+       os.system("cp %s/%s.pdf /home/ben/Downloads/foo.pdf" % (out_dir, tag))
+    ## END HACKS
     return
+
+def main_aggregate(argv):
+   raise NotImplementedError()
 
 def print_help():
     """ (-> Void)
@@ -51,11 +66,14 @@ def print_help():
     """
     print("Usage: ./summary FILE.rktd")
 
-### Entry point
+### Entry point, accepts a list of files.
 
 if __name__ == "__main__":
-    if len(sys.argv) == 2:
-        main(sys.argv[1])
+    if len(sys.argv) < 2:
+       print_help()
+    elif sys.argv[1] in ["-a", "--all", "--aggregate"]:
+       main_aggregate(sys.argv[2::])
     else:
-        print_help()
-
+       for fname in sys.argv[1::]:
+          main(fname)
+    
