4e1c86240c2360655da50335365fd364dbd76ae3
ben <types@ccs.neu.edu>
2015-06-19 16:59:34 -0400

    [more-inputs] quad, added sort-of-large text sample (typed is just very slow)

---
+++ quad/README.md
@@ -0,0 +1,7 @@
+quad
+====
+
+Benchmark renders a document from the source code `quick-sample.rkt`.
+
+The `base/` folder has alternate versions of `quick-sample.rkt`.
+To run the benchmark on these alternates, replace `quick-sample.rkt` with one of them.
--- quad/untyped/main.rkt
+++ quad/untyped/main.rkt
@@ -21,9 +21,11 @@
 
 ;; =============================================================================
 
+;; 137ms
 (parameterize ([world:quality-default world:draft-quality])
   (time
     (begin
       (define to (typeset (quick-sample)))
       (send (new pdf-renderer%) render-to-file to "../base/output.pdf")
       (void))))
+;; 630ms for heart-of-darkness
