271391d99de68e3fb887f83c11fa1900e7aca54d
Asumu Takikawa <asumu@ccs.neu.edu>
2015-07-09 06:27:40 -0400

    Add more benchmark scripts

---
+++ cluster/quad-benchmark-pre-zo-part-14.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+#BSUB -J QUAD-P14
+#BSUB -o quad_output_part_14_file
+#BSUB -e quad_error_part_14_file
+#BSUB -n 40
+#BSUB -R "span[ptile=40]"
+#BSUB -q ser-par-10g-2
+#BSUB -cwd /scratch/takikawa.a
+
+work=/scratch/takikawa.a/gradual-typing-performance
+
+#####################################################
+########DO NOT EDIT ANYTHING BELOW THIS LINE#########
+#####################################################
+cd $work
+tempfile1=hostlistrun
+tempfile2=hostlist-tcp
+echo $LSB_MCPU_HOSTS > $tempfile1
+declare -a hosts
+read -a hosts < ${tempfile1}
+for ((i=0; i<${#hosts[@]}; i += 2)) ; 
+do 
+   HOST=${hosts[$i]}
+   CORE=${hosts[(($i+1))]} 
+   echo $HOST:$CORE >> $tempfile2
+done
+#####################################################
+########DO NOT EDIT ANYTHING ABOVE THIS LINE#########
+#####################################################
+
+~/racket-6.2/bin/racket tools/run.rkt -o quad-results-part-14.rktd -m 41001 44000 -i 30 -j 39 quad
+
+cp quad-results-part-14.rktd ~/
+
+#####################################################
+########DO NOT EDIT ANYTHING BELOW THIS LINE#########
+#####################################################
+rm $work/$tempfile1
+rm $work/$tempfile2
+#####################################################
+########DO NOT EDIT ANYTHING ABOVE THIS LINE#########
+#####################################################
