0e1b685e338f249c7bef23c84540f0fdca73f8fc
ben <types@ccs.neu.edu>
2015-07-25 21:58:13 -0400

    [tracers] factorial example works

--- tracers/examples/factorial/fact.rkt
+++ tracers/examples/factorial/fact.rkt
@@ -1,15 +1,13 @@
 #lang racket/base
 
-(provide fact-naive fact-acc)
-;(require racket/contract)
-;(provide
-;  (contract-out
-;    [fact-naive (-> natural-number/c natural-number/c)]
-;    [fact-acc   (-> natural-number/c natural-number/c)])
-;)
+(provide fact-naive fact-acc my-num)
+
+(define my-num 4)
 
 (define (fact-naive n)
-  (if (< n 2) 1 (* n (fact-naive (sub1 n)))))
+  (if (< n 2)
+    1
+    (* n (fact-naive (sub1 n)))))
 
 (define (fact-acc n)
   (for/fold ([acc 1])
--- tracers/examples/factorial/main.rkt
+++ tracers/examples/factorial/main.rkt
@@ -1,13 +1,16 @@
 #lang typed/racket/base
 
-(require/typed "fact.rkt" [fact-naive (-> Natural Natural)] [fact-acc (-> Natural Natural)])
-;(require "fact.rkt")
+(require/typed "fact.rkt"
+  [my-num Natural]
+  [fact-naive (-> Natural Natural)]
+  [fact-acc (-> Natural Natural)])
 
 (define (main)
   (fact-naive 1)
   (fact-naive 10)
   (fact-acc 1)
   (fact-acc 10)
+  (+ 1 my-num)
   (void))
 
 (main)
