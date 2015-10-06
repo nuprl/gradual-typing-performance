401f5f7407491a62fca1022cc669e0e2dbfc3bd7
ben <types@ccs.neu.edu>
2015-10-05 20:24:40 -0400

    [artifact] unpack artifact + install pkgs

--- artifact/setup.sh
+++ artifact/setup.sh
@@ -2,6 +2,7 @@
 
 # Make sure there is a Desktop
 mkdir -p ~/Desktop
+rm -f ~/Desktop/*
 
 # Installing vagrant keys
 mkdir ~/.ssh
@@ -12,15 +13,16 @@ chmod 600 ~/.ssh/authorized_keys
 chown -R artifact ~/.ssh
 
 # Set up the artifact files
-cd ~
-tar xvf paper-archive.tar
-# FIXME: actually set up the benchmarks from the git repo archive
+cd ~/Desktop
+wget http://ccs.neu.edu/home/types/v0.0.tar.gz
+tar -xzf v0.0.tar.gz
+rm v0.0.tar.gz
 
-# Copy over the paper
-#cp paper-archive/submissions/camera-ready.pdf ~/Desktop/paper.pdf
+# TODO Copy over the paper
+# cp gradual-typing-performance-0.0/paper/draft.pdf ./is-sound-gradual-typing-dead.pdf
 
 # Install Racket
-# first download latest Racket
+# first download Racket v6.2
 wget http://mirror.racket-lang.org/installers/6.2/racket-6.2-x86_64-linux-ubuntu-precise.sh
 
 # Do a local install. A unix-style install is preferable in some ways, but the
@@ -32,14 +34,15 @@ export PATH=~/racket/bin:$PATH
 echo "export PATH=~/racket/bin:$PATH" >> ~/.bashrc
 
 # Install packages that are needed for the artifact
-# FIXME: install packages here
+yes | raco pkg install glob
+yes | raco pkg install ./gradual-typing-performance-0.0/tools/benchmark-util
 raco setup -D # avoid huge memory use from doc build
 
-# Create the scribble docs
-cd paper-archive/artifact
-scribble +m --htmls README.scrbl
-mv README ~/readme-folder
-ln -s ~/readme-folder/index.html ~/Desktop/README.html
+# TODO Create the scribble docs
+#cd TODO/
+#scribble +m --htmls README.scrbl
+#mv README ~/Desktop/readme-folder
+#ln -s ~/Desktop/readme-folder/index.html ~/Desktop/README.html
 
 # Configure XFCE, instead of directly configuring this put it in the
 # .bash_profile because the command won't work without X11 running.
@@ -67,7 +70,5 @@ echo "StartupNotify=false"        >> ~/Desktop/DrRacket.desktop
 chmod +x ~/Desktop/DrRacket.desktop
 
 # Cleanup
-cd ~
-#rm -r ~/paper-archive
-rm paper-archive.tar
+cd ~/Desktop
 rm racket-6.2-x86_64-linux-ubuntu-precise.sh
