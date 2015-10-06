f0e4b127247536d5fcce0d5411461f5201c40903
ben <types@ccs.neu.edu>
2015-10-06 19:19:14 -0400

    [data+code] check for invalid args

--- run.sh
+++ run.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-export NUMITERS=1
-export DATA=$1.rktd
-export LOG=$1.log
-export PNG=$1.png
+if [[ $1 ]]
+then
+    export NUMITERS=1
+    export DATA=$1.rktd
+    export LOG=$1.log
+    export PNG=$1.png
 
-echo "### Running benchmarks for '"$1"' ("$NUMITERS" iterations per configuration)"
-racket tools/setup-benchmark.rkt $1
-racket tools/run.rkt -i $NUMITERS -o $DATA $1 | tee $LOG
-racket tools/view.rkt -o $PNG $DATA
-echo "### Saved logfile to '"$LOG"'"
-echo "### Saved data to '"$DATA"'"
-echo "### Saved figure to '"$PNG"'"
+    echo "### Running benchmarks for '"$1"' ("$NUMITERS" iterations per configuration)"
+    racket tools/setup-benchmark.rkt $1
+    racket tools/run.rkt -i $NUMITERS -o $DATA $1 | tee $LOG
+    racket tools/view.rkt -o $PNG $DATA
+    echo "### Saved logfile to '"$LOG"'"
+    echo "### Saved data to '"$DATA"'"
+    echo "### Saved figure to '"$PNG"'"
+else
+    echo "Usage: run.sh BENCHMARK"
+fi
