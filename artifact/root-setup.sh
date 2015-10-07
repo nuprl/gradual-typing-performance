8e1016ee0f6d5838790452a4ce01f79c9f305d66
ben <types@ccs.neu.edu>
2015-10-07 15:16:04 -0400

    [artifact] ugh.... missing 'install'

--- artifact/root-setup.sh
+++ artifact/root-setup.sh
@@ -10,7 +10,7 @@ sudo apt-get -y -q install virtualbox-guest-utils virtualbox-guest-x11 virtualbo
 # Install necessary packages
 sudo apt-get -y -q install firefox vim xvfb git wget tar xfce4 lightdm mupdf
 # Install latex, for Scribble
-sudo apt-get -y -q texlive-latex-base texlive-fonts-recommended texlive-fonts-extra texlive-latex-extra pgf
+sudo apt-get -y -q install texlive-latex-base texlive-fonts-recommended texlive-fonts-extra texlive-latex-extra pgf
 
 # Add an XSession session for lightdm
 sudo echo "[Desktop Entry]"         > /usr/share/xsessions/custom.desktop
