748ec7285bc9ee436acd60ff5b725dcab4b6246e
ben <types@ccs.neu.edu>
2015-10-10 04:06:46 -0400

    [artifact] drop trailing / in run script

--- run.sh
+++ run.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 
-if [[ $1 ]]
-then
-    export NUMITERS=1
-    export DATA=$1.rktd
-    export LOG=$1.log
-    export PNG=$1.png
+export NUMITERS=1
+export TARGET=${1%/}
+export DATA=$TARGET.rktd
+export LOG=$TARGET.log
+export PNG=$TARGET.png
 
-    echo "### Running benchmarks for '"$1"' ("$NUMITERS" iterations per configuration)"
-    racket tools/setup-benchmark.rkt $1
-    racket tools/run.rkt -i $NUMITERS -o $DATA $1 | tee $LOG
-    racket tools/view.rkt -o $PNG $DATA
-    echo "### Saved logfile to '"$LOG"'"
-    echo "### Saved data to '"$DATA"'"
-    echo "### Saved figure to '"$PNG"'"
-else
-    echo "Usage: run.sh BENCHMARK"
-fi
+echo "### Running benchmarks for '"$TARGET"' ("$NUMITERS" iterations per configuration)"
+racket tools/setup-benchmark.rkt $TARGET
+racket tools/run.rkt -i $NUMITERS -o $DATA $TARGET | tee $LOG
+racket tools/view.rkt -o $PNG $DATA
+echo "### Saved logfile to '"$LOG"'"
+echo "### Saved data to '"$DATA"'"
+echo "### Saved figure to '"$PNG"'"
