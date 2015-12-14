5db35220d4c43e0d1b9959d27f6bb66d30335f2a
ben <types@ccs.neu.edu>
2015-12-14 16:27:23 -0500

    [class] wrapper counting working okay

--- class/microbench/exponential-wrap.rkt
+++ class/microbench/exponential-wrap.rkt
@@ -1,16 +1,27 @@
 #lang racket
 
-;; Exponential slowdown from class contracts.
+;; Exponential slowdown from object contracts.
+;;
+;; (Class contracts seem fine, so far)
 
 ;; -----------------------------------------------------------------------------
 
 (module+ main
-  (define lo 6)
-  (define hi 9)
+  (require "count-chaps.rkt")
+  (define lo 1)
+  (define hi 6)
 
   (for ([i (in-list (for/list ([j (in-range lo hi)]) (* 5 j)))])
     (displayln i)
-    (time (main i))))
+    (time (main i))
+    (count-chaps))
+
+  ;(time (main 1))
+  ;(time (main 2))
+  ;(time (main 3))
+  ;(time (main 25))
+  ;(count-chaps)
+)
 
 ;; -----------------------------------------------------------------------------
 
@@ -36,19 +47,36 @@
 
   (: obj ComplexEnough)
   (define obj (new c%))
+  (: obj2 ComplexEnough)
+  (define obj2 (new c%))
 
   ;; --- 2. Create a simple function on objects
+  (: mymix (-> ComplexEnough% ComplexEnough%))
+  (define (mymix x) x)
+
   (: myid (-> ComplexEnough ComplexEnough))
   (define (myid x) x)
 
-  (provide obj myid))
+  (provide c% obj obj2 myid mymix))
 
 ;; --- 3. Repeatedly apply the function to an object
 (require 't1)
-(define (main M)
+
+(define (check-class M)
   (for/fold
-      ([obj obj])
+      ([c c%])
       ([i (in-range M)])
-    (myid obj))
+    (mymix c))
   (void))
+
+(define (check-object M)
+  (for/fold
+      ([obj1 obj]
+       [obj2 obj2])
+      ([i (in-range M)])
+    (values (myid obj1) (myid obj2)))
+  (void))
+
+(define (main M)
+  (check-class M))
 
