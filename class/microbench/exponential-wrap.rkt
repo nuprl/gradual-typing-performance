c620c7de4eafca3db7193cc88a054b3883469534
ben <types@ccs.neu.edu>
2015-12-28 14:42:29 -0500

    [class] simplify exponential-wrapping class contract

--- class/microbench/exponential-wrap.rkt
+++ class/microbench/exponential-wrap.rkt
@@ -7,20 +7,13 @@
 ;; -----------------------------------------------------------------------------
 
 (module+ main
-  (require "count-chaps.rkt")
-  (define lo 1)
-  (define hi 6)
+  (require
+    benchmark-util
+    racket/sandbox)
 
-  (for ([i (in-list (for/list ([j (in-range lo hi)]) (* 5 j)))])
-    (displayln i)
-    (time (main i))
-    (count-chaps))
+  (with-limits 1 1
+    (time (main 99)))
 
-  ;(time (main 1))
-  ;(time (main 2))
-  ;(time (main 3))
-  ;(time (main 25))
-  ;(count-chaps)
 )
 
 ;; -----------------------------------------------------------------------------
@@ -30,13 +23,7 @@
   ;; --- 1. Create a "complicated-enough" object type
   (define-type ComplexEnough%
     (Class
-      (lam
-        ;(-> (Class) (Class))            ;; No problem
-        ;(-> C C)                        ;; Problem
-        ;(-> Any ComplexEnough)          ;; Less-serious problem
-        ;(-> ComplexEnough Any)          ;; Problem
-        (-> ComplexEnough ComplexEnough) ;; Problem
-      )))
+      (lam (-> ComplexEnough ComplexEnough))))
   (define-type ComplexEnough (Instance ComplexEnough%))
 
   (: c% ComplexEnough%)
@@ -47,36 +34,20 @@
 
   (: obj ComplexEnough)
   (define obj (new c%))
-  (: obj2 ComplexEnough)
-  (define obj2 (new c%))
 
   ;; --- 2. Create a simple function on objects
-  (: mymix (-> ComplexEnough% ComplexEnough%))
-  (define (mymix x) x)
-
   (: myid (-> ComplexEnough ComplexEnough))
   (define (myid x) x)
 
-  (provide c% obj obj2 myid mymix))
+  (provide c% obj myid))
 
 ;; --- 3. Repeatedly apply the function to an object
 (require 't1)
 
-(define (check-class M)
-  (for/fold
-      ([c c%])
-      ([i (in-range M)])
-    (mymix c))
-  (void))
-
-(define (check-object M)
-  (for/fold
-      ([obj1 obj]
-       [obj2 obj2])
-      ([i (in-range M)])
-    (values (myid obj1) (myid obj2)))
-  (void))
-
 (define (main M)
-  (check-class M))
+  (for/fold
+      ([obj1 obj])
+      ([i (in-range M)])
+    (myid obj1))
+  (void))
 
