20fb8137ec1dd7ea69d482fbd9a955ab465d7acc
ben <benjaminlgreenman@gmail.com>
2016-02-15 21:18:19 -0500

    [tools] checkpoint: tracing sometimes, sometimes accurately

--- tools/summarize/trace.rkt
+++ tools/summarize/trace.rkt
@@ -10,10 +10,10 @@
 (require
   gtp-summarize/modulegraph
   glob
-  (only-in racket/string string-replace)
+  (only-in racket/string string-replace string-prefix? string-split)
   (only-in racket/file delete-directory/files)
   (only-in racket/port copy-port)
-  (only-in racket/system system)
+  (only-in racket/system system process)
 )
 
 ;; =============================================================================
@@ -21,8 +21,13 @@
 (define TRACE-PREFIX 'trace:)
 ;; Prefix for indirected identifiers
 
+(define TRACE-LOG-PREFIX "[TRACE]")
+
 (define TRACE-TMP ".trace.tmp")
 ;; Temporary file to use for search/replace
+
+(define *RKT-BIN* (make-parameter ""))
+;; Location of Racket binaries
 
 (define *TEX-OUT* (make-parameter "graph.tex"))
 ;(define *STD-OUT* (make-parameter #f))
@@ -36,7 +41,7 @@
 
 ;; =============================================================================
 
-;; (define-type DynamicBoundary (List String String (Listof (Pairof Provided Natural))))
+;; (define-type DynamicBoundary (List String String (Listof (Pairof Symbol Natural))))
 ;; (define-type Dynamic-ModuleGraph dynamic-modulegraph)
 
 (struct dynamic-modulegraph (
@@ -117,7 +122,7 @@
         (for ([p (in-list provided*)])
           (define id (provided->symbol p))
           (define id/trace (format "~a~a" TRACE-PREFIX id))
-          (define msg (format "[TRACE]\t~a\t~a\t~a" to from id))
+          (define msg (format "~a\t~a\t~a\t~a" TRACE-LOG-PREFIX to from id))
           (printf "(define-syntax (~a stx)\n" id)
           (printf "  (syntax-parse stx\n")
           (printf "   [(_ arg* ...)\n")
@@ -134,7 +139,8 @@
       (string-append "boundary/" boundary-filename))
     (void))
   ;; -- Copy both files
-  (copy-all (string-append trace-str "/../both") trace-str #:glob "*.rkt"))
+  (copy-all (string-append trace-str "/../both") trace-str #:glob "*.rkt")
+  trace-dir)
 
 ;; (: string-replace/file (-> Path-String String String Void))
 (define (string-replace/file fname from to)
@@ -169,15 +175,74 @@
         (delete-directory/files trace-dir))
       (setup-trace-dir trace-dir boundary*))))
 
+;; Create a hash from a list of boundaries.
+;; Return the hash and an increment procedure.
+;; (-> (Listof Boundary) (Values DynHash (-> String String Symbol Void)))
+(define (boundary*->dyn-hash b*)
+  (define H
+    (for/hash ([b (in-list b*)])
+      (define-values (to from provided*) (apply values b))
+      (define p-hash
+        (make-hasheq
+          (for/list ([p (in-list provided*)]) (cons (provided->symbol p) 0))))
+      (values (cons to from) p-hash)))
+  (define (H++ to from p)
+    (define H1 (hash-ref H (cons to from) (lambda () #f)))
+    (define old-val (and H1 (hash-ref H1 p (lambda () #f))))
+    (if (and H1 old-val)
+      (hash-set! H1 p (+ old-val 1))
+      (if old-val
+        (printf "Warning: no match for keys {to : ~a} {from : ~a}\n" to from)
+        (printf "Warning: no value for key {to : ~a} {from : ~a} {id : ~a}\n" to from p))))
+  (values H H++))
+
+;; (-> DynHash (Listof DynamicBoundary))
+(define (dyn-hash->dyn-boundary* H)
+  (for/list ([(to+from p*) (in-hash H)])
+    (list (car to+from)
+          (cdr to+from)
+          (sort
+            (for/list ([(p v) (in-hash p*)])
+              (cons p v))
+            symbol<?
+            #:key car))))
+
+;; Run a program, with tracing
+;; (-> Path-String (Listof Boundary) (Listof DynamicBoundary))
+(define (trace-run dir b*)
+  (define dir-str (if (string? dir) dir (path->string dir)))
+  (define main-file (string-append dir-str "/main.rkt"))
+  (define-values (H H++) (boundary*->dyn-hash b*))
+  ;; -- compile the project
+  (unless (system (format "~araco make ~a" (*RKT-BIN*) main-file))
+    (raise-user-error 'trace "Failed to compile ~a" main-file))
+  ;; -- run a subprocess, collect output
+  (define-values (stdout stdin pid stderr ping)
+    (apply values (process (format "~aracket ~a" (*RKT-BIN*) main-file))))
+  (for ([line (in-lines stdout)])
+    (cond
+     [(string-prefix? line TRACE-LOG-PREFIX)
+      (apply H++ (log->data line))]
+     [else
+      (void)]))
+  ;; -- close ports
+  (close-input-port stdout)
+  (close-output-port stdin)
+  (close-input-port stderr)
+  ;; -- immutable output
+  (dyn-hash->dyn-boundary* H))
+
+(define (log->data str)
+  (define-values (tag to from id payload) (apply values (string-split str "\t")))
+  (list to from (string->symbol id)))
+
 ;; (: directory->dmg (-> Path-String Dynamic-ModuleGraph))
 (define (directory->dmg dir)
   (define MG (from-directory dir))
   (define B* (boundaries MG))
   (define trace-dir (setup-trace-dir/check dir B*))
-
-  ;; TODO
-
-  (raise-user-error 'nope))
+  (define dyn-B* (trace-run trace-dir B*))
+  (dynamic-modulegraph MG dyn-B*))
 
 (define (project-name->dmg name)
   (directory->dmg (infer-project-dir name)))
@@ -188,15 +253,21 @@
   ;; Print title
   (define title (format "ModuleGraph for ~a:" (dmg->project-name dmg)))
   (displayln title)
-  (displayln (make-string (string-length title) "="))
+  (displayln (make-string (string-length title) #\=))
   (newline)
   ;; For each module, print info about its requires
   (for ([to (in-list (dmg->module-names dmg))])
     (printf "Requires for: ~a.rkt\n" to)
     (define indent "  - ")
     (for ([b (in-list (to->dyn-boundary* dmg to))])
-      (printf "  - ~a : ~a\n" (dyn-boundary-from b) (dyn-boundary-count b)))
+      (printf "  - ~a.rkt : ~a\n" (dyn-boundary-from b) (dyn-boundary-count b)))
     (newline)))
+
+;; (-> String Void)
+(define (assert-rkt-bin r)
+  (unless (and (file-exists? (string-append r "/racket"))
+               (file-exists? (string-append r "/raco")))
+    (raise-user-error 'trace "Directory '~a' does not have 'racket' and 'raco' executables" r)))
 
 ;; =============================================================================
 
@@ -207,6 +278,8 @@
    #:once-each
    [("--tex") tex "File to store .tex output to" (*TEX-OUT* tex)]
    [("-q" "--quiet") "Run quietly" (*VERBOSE* #f)]
+   [("-r" "--racket") r "Run quietly" (and (assert-rkt-bin r)
+                                           (*RKT-BIN* (string-append r "/")))]
    #:args (PROJECT-NAME)
    (define DMG (project-name->dmg PROJECT-NAME))
    (print-dmg DMG))
@@ -244,4 +317,40 @@
     '("the" "quick" "brown fox" "jumped over" "the" "lazy" "d og")
     "d o"
     "yolo")
+
+  (let ([provided* (list (list "toA" "fromA" (list (provided 'A1 #f #f)
+                                                   (provided 'A2 #f #f)
+                                                   (provided 'A3 #f #f)))
+                         (list "toB" "fromB" (list (provided 'B1 #f #f))))])
+    (let-values ([(H H++) (boundary*->dyn-hash provided*)])
+      (check-equal? (hash-count H) 2)
+      (check-equal?
+        (hash-count (hash-ref H (cons "toA" "fromA") (lambda () (error 'testfailed))))
+        3)
+      ;; -- check value before & after incrementing
+      (check-equal?
+        (hash-ref
+          (hash-ref H (cons "toB" "fromB") (lambda () (error 'testfailed)))
+          'B1)
+        0)
+      (H++ "toB" "fromB" 'B1)
+      (check-equal?
+        (hash-ref
+          (hash-ref H (cons "toB" "fromB") (lambda () (error 'testfailed)))
+          'B1)
+        1)
+      ;; -- check dynhash->dynboundary
+      (check-equal?
+        (sort (dyn-hash->dyn-boundary* H) string<? #:key car)
+        '(("toA" "fromA" ((A1 . 0) (A2 . 0) (A3 . 0)))
+          ("toB" "fromB" ((B1 . 1)))))
+      ))
+
+  (define-syntax-rule (check-log->data* [in out] ...)
+    (begin (check-equal? (log->data in) out) ...))
+  (check-log->data*
+   ["[TRACE]\tto\tfrom\tid\twhateverelse"
+    '("to" "from" id)]
+   ["A\tB\tC\tD\tE\n"
+    '("B" "C" D)])
 )
