588883f29c78ada77b0ab2139f2bc8201a7bd9bc
ben <benjaminlgreenman@gmail.com>
2016-02-23 01:11:58 -0500

    [quad] renamed csp/main, to avoid conflict with my sed script :/

--- benchmarks/quadMB/typed/quad-main.rkt
+++ benchmarks/quadMB/typed/quad-main.rkt
@@ -83,7 +83,7 @@
  (slice-at (All (A) (case-> ((Listof A) Positive-Integer -> (Listof (Listof A)))
                    ((Listof A) Positive-Integer Boolean -> (Listof (Listof A)))))))
 ;; bg: should maybe import this
-(require/typed "../base/csp/main.rkt"
+(require/typed "../base/csp/csp.rkt"
   [problem%  (Class (init-field [solver Any])
   (field [_solver Any])
   (field [_variable-domains Any])
