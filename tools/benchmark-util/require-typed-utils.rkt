8126be897b1f41bb6a5a148cd73a6e2e118b2a92
Max New <maxsnew@gmail.com>
2016-02-23 17:50:26 -0500

    Clean require/typed/check and dummy require/adapted

--- tools/benchmark-util/require-typed-utils.rkt
+++ tools/benchmark-util/require-typed-utils.rkt
@@ -8,6 +8,10 @@
   require/typed/check
   ;; Same syntax as require/typed, but does not install contracts
   ;;  if the current module AND providing module are typed.
+  ;;  Additionally, can be overidden by a benchmark-config.rktd file
+
+  require/adapted
+  ;; Same as typed require, but can be overriden by a benchmark-config.rktd file
 
   require/typed/if
   ;; (require/typed/if T E)
@@ -23,29 +27,37 @@
   (prefix-in typed: (only-in typed/racket require))
 )
 
-;; =============================================================================
+(begin-for-syntax
+  (define-syntax-rule (they-are-typed m)
+    (module->language-info (syntax->datum #'m) #t))
+  (define (i-am-typed) (syntax-local-typed-context?)))
 
+;; =============================================================================
+;; TODO: check for overrides in this and require/adapted
 (define-syntax (require/typed/check stx)
   (syntax-parse stx 
     #:literals (prefix-in)
     [(_ (prefix-in p m:str) rt-clause ...)
      (cond 
-       [(not (syntax-local-typed-context?))
+       [(not (i-am-typed))
         #'(require (prefix-in p m))]
-       [(and (syntax-local-typed-context?)
-             (module->language-info (syntax->datum #'m) #t))
+       [(they-are-typed m)
         #'(typed:require (prefix-in p m))]
        [else 
         #'(require/typed m rt-clause ...)])]
     [(_ m:str rt-clause ...)
      (cond 
-       [(not (syntax-local-typed-context?))
-        #'(require m)]
-       [(and (syntax-local-typed-context?)
-             (module->language-info (syntax->datum #'m) #t))
-        #'(typed:require m)]
+       [(not (i-am-typed)) #'(require m)]
+       [(they-are-typed m) #'(typed:require m)]
        [else 
         #'(require/typed m rt-clause ...)])]))
+
+(define-syntax (require/adapted stx)
+  (syntax-parse stx
+    #:literals (prefix-in)
+    [(_ (prefix-in p m:str) rt-clause ...)
+     #'(require/typed m rt-clause ...)]
+    [(_ m:str rt-clause ...)]))
 
 (define-syntax (require/typed/if stx)
   (syntax-parse stx 
