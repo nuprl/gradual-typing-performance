2a7a11083a4cf17a4426bf295cba371d7e89fe05
ben <types@ccs.neu.edu>
2016-03-25 17:07:31 -0400

    ** add benchmarks test to travis

--- tools/summarize/test/mini-quadMB/base/csp/helper.rkt
+++ tools/summarize/test/mini-quadMB/base/csp/helper.rkt
@@ -1,7 +1,6 @@
 #lang racket/base
 (require racket/class racket/list (for-syntax racket/base racket/syntax))
 (provide (all-defined-out))
-(require rackunit)
 
 (define-syntax-rule (forever expr ...)
   (for ([i (in-naturals)])
@@ -21,10 +20,6 @@
                   (send/apply this proc-name args))])
               proc-name))
 
-(define-simple-check (check-hash-items h1 h2)
-  (for/and ([(k1 v1) (in-hash h1)])
-    (equal? (hash-ref h2 k1) v1)))
-
 (define (list-comparator xs ys)
   ;; For use in sort. Compares two lists element by element.
   (cond
@@ -39,13 +34,6 @@
               [(and (string? x) (string? y)) (string<? x y)]
               [else (error 'list-comparator (format "Can’t compare ~v and ~v" x y))]))]))
 
-(module+ test 
-  (check-false (list-comparator null null))
-  (check-false (list-comparator (range 2) (range 2)))
-  (check-true (list-comparator (range 2) (range 4)))
-  (check-false (list-comparator (range 4) (range 2)))
-  (check-true (list-comparator '(1 1 "a") '(1 1 "b")))
-  (check-true (list-comparator '(1 1 a) '(1 1 b))))
 
 (define-syntax-rule (car-pop! xs)
   (let ([i (car xs)])
@@ -57,24 +45,12 @@
       (set! xs (drop-right xs 1))
       i))
 
-(module+ test
-  (let ([xs '(1 2 3)])
-    (check-equal? (py-pop! xs) 3)
-    (check-equal? xs '(1 2))))
 
 (define-syntax-rule (py-append! xs x)
   (set! xs `(,@xs ,x)))
 
 (define-syntax-rule (py-extend! xs x)
   (set! xs `(,@xs ,@x)))
-
-(module+ test
-  (let ([xs '(1 2 3)])
-    (py-append! xs (range 2))
-    (check-equal? xs '(1 2 3 (0 1))))
-  (let ([xs '(1 2 3)])
-    (py-extend! xs (range 2))
-    (check-equal? xs '(1 2 3 0 1))))
 
 
 (define (word-value . xs)
