3e78ff390be0eea1b6ea88b72aedc811573edda4
ben <benjaminlgreenman@gmail.com>
2016-04-14 16:13:10 -0400

    [jfp] make tick lines optional

--- tools/summarize/lnm-parameters.rkt
+++ tools/summarize/lnm-parameters.rkt
@@ -99,11 +99,13 @@
 (defparam *CUTOFF-WIDTH* Nonnegative-Real thin)
 
 (defparam *TICK-SIZE* Natural 4) ;; Dude IDK
-(defparam *X-NUM-TICKS* Natural 5)
-(defparam *X-TICKS* (U #f (Listof Exact-Rational)) #f) ;; Takes precedence over num-ticks
 (defparam *X-MINOR-TICKS* (U #f (Listof Real)) #f)
-(defparam *Y-NUM-TICKS* Natural 6)
+(defparam *X-NUM-TICKS* Natural 5)
+(defparam *X-TICK-LINES?* Boolean #f)
+(defparam *X-TICKS* (U #f (Listof Exact-Rational)) #f) ;; Takes precedence over num-ticks
 (defparam *Y-MINOR-TICKS* (U #f (Listof Real)) #f)
+(defparam *Y-NUM-TICKS* Natural 6)
+(defparam *Y-TICK-LINES?* Boolean #f)
 (define-type Y-Style (U 'count '%))
 (defparam *Y-STYLE* Y-Style 'count)
 
