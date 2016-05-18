c1b4d40302be5d4f85a9bbdec1aa6b6054f23df8
Ben Greenman <benjaminlgreenman@gmail.com>
2016-05-18 17:15:32 -0400

    ** update run-all.sh for barrier-run

--- run-all.sh
+++ run-all.sh
@@ -5,21 +5,25 @@ read -p "Are you sure you want to continue? " -n 1 -r
 echo ""
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-  for project in \
-    acquire \
-    forth \
-    fsm \
-    fsmoo \
-    zordoz.6.2 \
-    zombie \
-    tetris \
-    synth \
-    kcfa \
-    gregor \
-    quadBG \
-    quadMB \
-    sieve \
-  ; do
-    sh run.sh benchmarks/$project
-  done
+  raco gtp-run -v '6.5' \
+    benchmarks/morsecode \
+    benchmarks/fsm \
+    benchmarks/forth \
+    benchmarks/take5 \
+    benchmarks/zordoz.6.3 \
+    benchmarks/zombie \
+    benchmarks/suffixtree \
+    benchmarks/snake \
+    benchmarks/tetris \
+    benchmarks/synth \
+    benchmarks/kcfa \
+    benchmarks/lnm \
+    benchmarks/mbta \
+    benchmarks/acquire \
+    benchmarks/dungeon
 fi
+#    fsmoo \
+#    quadBG \
+#    quadMB \
+#    gregor \
+#    sieve
