6ac1752f4f70347b548511675ad7579943ebe10e
Ben Greenman <benjaminlgreenman@gmail.com>
2016-06-08 17:19:42 -0400

    [zordoz.6.5] new bytecode format

---
+++ benchmarks/zordoz.6.5/typed/main.rkt
@@ -0,0 +1,28 @@
+#lang typed/racket/base
+
+(require benchmark-util)
+
+(require/typed/check "zo-shell.rkt"
+  [init (-> (Vectorof String) Void)])
+
+;; Stress tests: search entire bytecode for the fairly-common branch struct
+(define SELF-TEST '("../base/zo-shell.zo" "../base/zo-find.zo" "../base/zo-string.zo" "../base/zo-transition.zo"))
+(define (self-test)
+  (for ([b SELF-TEST]) (init (vector b "branch"))))
+
+(define SMALL-TEST "../base/hello-world.zo")
+(define (small-test)
+  (init (vector SMALL-TEST "branch")))
+
+(define LARGE-TEST "../base/streams.zo")
+(define (large-test)
+  (init (vector LARGE-TEST "branch")))
+
+;; -----------------------------------------------------------------------------
+
+(define-syntax-rule (main test)
+  (with-output-to-file "/dev/null" test #:exists 'append))
+
+(time (main self-test)) ; 1330ms
+;(time (main small-test)) ;
+;(time (main large-test)) ;
