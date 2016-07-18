5447da14eeb5d00d202f7361448399160f66268f
Ben Greenman <benjaminlgreenman@gmail.com>
2016-07-18 10:20:00 -0400

    [karst] run-all script

---
+++ karst/run-all.sh
@@ -0,0 +1,25 @@
+VERSION=$1
+
+#          gregor \
+#          quadBG \
+#          quadMB \
+for BM in acquire \
+          dungeon \
+          forth \
+          fsm \
+          fsmoo \
+          kcfa \
+          lnm \
+          mbta \
+          morsecode \
+          sieve \
+          snake \
+          stack \
+          suffixtree \
+          synth \
+          take5 \
+          tetris \
+          zombie \
+          zordoz.6.2; do # zordoz.6.3 zordoz.6.5
+sh run-benchmark.sh ${VERSION} ${BM}
+done;
