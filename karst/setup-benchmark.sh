6ca15134f61c043c022a5eac469667fa8e1c6965
Ben Greenman <benjaminlgreenman@gmail.com>
2016-07-19 11:29:57 -0400

    [karst] setup all benchmarks script

---
+++ karst/setup-benchmark.sh
@@ -0,0 +1,30 @@
+GTP="/N/u/bengree/Karst/gradual-typing-performance"
+RKT="/N/u/bengree/Karst/racket/6.5/bin"
+
+find ${GTP} -name "benchmark" | xargs rm -r
+
+for BM in acquire \
+dungeon \
+gregor \
+forth \
+fsm \
+fsmoo \
+kcfa \
+lnm \
+mbta \
+morsecode \
+quadBG \
+quadMB \
+sieve \
+snake \
+stack \
+suffixtree \
+synth \
+take5 \
+tetris \
+zombie \
+zordoz.6.2 \
+zordoz.6.3 \
+zordoz.6.5; do
+${RKT}/racket ${GTP}/tools/setup-benchmark.rkt ${GTP}/benchmarks/${BM}
+done;
