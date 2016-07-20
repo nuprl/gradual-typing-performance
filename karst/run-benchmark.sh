43bd89b536baa0c4af7028b9a24668429ef140f0
Ben Greenman <benjaminlgreenman@gmail.com>
2016-07-20 04:56:37 -0400

    [karst] export environment to qscript, but only halfway done

--- karst/run-benchmark.sh
+++ karst/run-benchmark.sh
@@ -1,7 +1,7 @@
 # Just run qsub with the qscript
 if [ $# -eq 1 ]; then
-  RACKET_VERSION=$1
-  qsub -k o -l nodes=1:ppn=16,walltime=24:00:00 qscript.sh
+  export RACKET_VERSION=$1
+  qsub -V -k o -l nodes=1:ppn=16,walltime=24:00:00 qscript.sh
 else
   echo "Usage: run-benchmark.sh <RACKET-VERSION>"
 fi
