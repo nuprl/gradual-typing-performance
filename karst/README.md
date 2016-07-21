68d15d3883561691c7a4ae41019def50d066ee91
Ben Greenman <benjaminlgreenman@gmail.com>
2016-07-21 17:57:09 -0400

    [karst] documented

--- karst/README.md
+++ karst/README.md
@@ -3,7 +3,22 @@ karst
 
 Scripts for the Karst cluster.
 
-- `run-benchmark.sh <RKT-VERSION> <BENCHMARK-NAME>` for example `sh run-benchmark.sh 6.5 morsecode`.
-  Sets up a benchmark and Generates batch commands to run all configurations.
+- `again.rkt` resume where we left off yesterday
+  - Writes to: `.again.rktd`
+- `check-status.sh <RKT> <N>` count the number of output files for Racket version `<RKT>` with `<N>` iterations completed.
+- `collect.rkt <VERSION>` gather results for one Racket version.
+   Use `racket collect.rkt --help` to see options.
+- `run-benchmark.sh` puts ONE node to work running configurations
+- `setup-benchmark.sh` clears the old benchmark directory, sets up everything
+- `qscript.sh` instructions for a node. This is what `run-benchmark.sh` starts running.
 
+
+Getting Started
+---
+
+Workflow for a new cluster account:
+1. Login to the cluster, download/install Racket, download/install this repo
+2. Run the setup script
+3. Run the `run` script for as many nodes as possible
+4. ... wait 24 hours, repeat step 3 until done
 
--- karst/qscript.sh
+++ karst/qscript.sh
@@ -1,65 +1,99 @@
-# This script assumes that all configurations for all interesting benchmarks
-# have already been generated.
+# Usage: NONE
+#  Do not call this script directly. Just let `run-benchmark.sh` do it.
 
-# -----------------------------------------------------------------------------
-# --- Initialize variables
+# === HIGH-LEVEL BEHAVIOR =====================================================
+# Loops forever,
+# - picking a random configuration that has not been run yet (this round)
+# - runs that configuration 1 time, records the result
 
-RACKET_VERSION="6.2"
+# If interrupted by the server, we lose only 1 run & can clean the data after.
+
+# === STATE ===================================================================
+# All state is kept on the filesystem. In particular:
+# - Input variable APPEND (argument $2) is the number of lines we expect
+#    all output files to have at the start of "this round".
+#   "This round" is over when all configurations have an output file with
+#    APPEND+1 lines
+# - Each configuration folder has an output file (variable OUT) marked with
+#    a Racket version number.
+#   This is the file we append to (or create if it's the first round).
+# - A file LOCKFILE prevents race conditions
+
+# === LOW-LEVEL BEHAVIOR ======================================================
+# Here is what really happens:
+# - Count the number of configuration directories that have either
+#    (1) no output file or
+#    (2) an output file with APPEND lines
+# - Draw a random number `n` in range
+# - Iterate over the configuration directories again, pick the `n`-th
+#   (Make sure this directory still needs to run -- another node might
+#    have finished it in the meantime.)
+# - "Claim" the configuration by writing a lock file
+# - Compile & run the configuration, append results to file
+
+# Assumes that all configurations for all interesting benchmarks
+#  have already been generated.
+
+# =============================================================================
+# --- start here --------------------------------------------------------------
+# =============================================================================
+
+RACKET_VERSION="6.2" # TODO should be $1
 # String, a valid Racket version like "6.3"
 
-APPEND=0 #$2
+APPEND=0 # TODO should be $2
 # Natural, the number of iterations we should have already finished.
 #   A "round" is over when all configs have (${APPEND} + 2) lines of output.
 
-EXPECTED_LINES=$((APPEND + 2))
-
 GTP="/N/u/bengree/Karst/gtp/benchmarks"
+# Location of the GTP repo, used to find configurations
+
 RKT="/N/u/bengree/Karst/racket/${RACKET_VERSION}/bin"
+# Location of Racket executables, for this version
+
 OUT="output-${RACKET_VERSION}.txt"
+# Name of output file. Every configuration directory gets a file named ${OUT}
 
 THE="benjaminlgreenman"
 BOSS="gmail"
 THEBOSS="${THE}@${BOSS}.com"
+# For sending emails
 
 LOCKFILE="mutex.lock"
+# For claiming a configuration directory
 
 # -----------------------------------------------------------------------------
 
-while [ 1 ]; do
-  # --- Pick a random configuration to run, exit if there aren't any left
-  NUM_CONFIGS_LEFT=0
+while [ 1 ]; do # --- forever
+  NUM_CONFIGS_LEFT=0 # --- Count remaining configuration directories
   if [ ${APPEND} -eq 0 ]; then
-    # echo "APPEND 0"
+    # --- Count configs with NO output file
     for DIR in $(find ${GTP} -name "config*"); do
-      # echo "DIR = ${DIR}"
       cd ${DIR};
       if [ ! -f ${OUT} ]; then
-        # echo "OUT does not exist"
         NUM_CONFIGS_LEFT=$((NUM_CONFIGS_LEFT + 1));
       fi
       cd -;
     done
   else
+    # --- Count configs with an output file with the right number of lines
     for FN in $(find ${GTP} -name ${OUT}); do
       if [ $(wc -l < ${FN}) -eq ${APPEND} ]; then
         NUM_CONFIGS_LEFT=$((NUM_CONFIGS_LEFT + 1));
       fi
     done
   fi
-  # echo "NCL = ${NUM_CONFIGS_LEFT}"
   if [ ${NUM_CONFIGS_LEFT} -eq 0 ]; then
+    # --- If no config directories, we must be done
     echo "DONE"
     echo "All configurations have ${APPEND} lines" | mail -s "[GTP:KARST] done" ${THEBOSS}
     break
   else
-    # --- Choose one of the configs.
-    #     RACE CONDITION: it's possible that the index isn't valid now
-    #                      because another node also picked it.
-    #                     If so, just restart the while loop.
+    # --- Choose a random config, indexed by their "find-order"
     CONFIG_INDEX=$(( ( RANDOM % NUM_CONFIGS_LEFT ) + 1 ))
-    # echo "CONFIG INDEX = ${CONFIG_INDEX}"
     CONFIG_FILE=""
     if [ ${APPEND} -eq 0 ]; then
+      # --- Again, look for files with no output
       for DIR in $(find ${GTP} -name "config*"); do
         if [ ! -f "${DIR}/${OUT}" ]; then
           CONFIG_INDEX=$((CONFIG_INDEX - 1))
@@ -70,6 +104,7 @@ while [ 1 ]; do
         fi
       done
     else
+      # --- Otherwise, look for files with the right number of lines
       for FN in $(find ${GTP} -name ${OUT}); do
         if [ $(wc -l < ${FN}) -eq ${APPEND} ]; then
           CONFIG_INDEX=$((CONFIG_INDEX - 1))
@@ -80,9 +115,8 @@ while [ 1 ]; do
         fi
       done
     fi
-    # echo "CFG FILE = ${CONFIG_FILE}"
-
-    # --- Check if someone else already picked the file, otherwise do nothing
+    # --- Almost time to run something,
+    # --- first be sure the config isn't taken by another node
     if [ ! -z ${CONFIG_FILE} ]; then
       cd $(dirname ${CONFIG_FILE});
       if [ ! -f ${LOCKFILE} ]; then
