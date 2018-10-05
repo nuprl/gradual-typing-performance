169c75ea6682216354e1bbf180dccc495f2d779d
Sam Tobin-Hochstadt <samth@cs.indiana.edu>
2018-10-05 12:52:19 -0400

    remove quadMB.

--- benchmarks/quadMB/typed/exceptions.rkt
+++
@@ -1,4 +0,0 @@
-#lang typed/racket/base
-(provide default-exceptions)
-(: default-exceptions (Listof Symbol))
-(define default-exceptions '(as-so-ciate as-so-ciates dec-li-na-tion oblig-a-tory phil-an-thropic present presents project projects reci-procity re-cog-ni-zance ref-or-ma-tion ret-ri-bu-tion ta-ble))
